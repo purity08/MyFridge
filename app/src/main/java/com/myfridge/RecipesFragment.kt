a6208a3636fe4492c688736f7e2ee3cec82560c9
Nagulevich Maxim <mnagulevich14@gmail.com>
2021-07-27 12:37:12 +0300

    add fragment for recipes

---
+++ app/src/main/java/com/myfridge/RecipesFragment.kt
@@ -0,0 +1,11 @@
+package com.myfridge
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+
+class RecipesFragment : Fragment(R.layout.fragment_recipes) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
