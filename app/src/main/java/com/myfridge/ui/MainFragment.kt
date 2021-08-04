04332565c37eefdfcc86438e88a9802ef397db1c
Nagulevich Maxim <mnagulevich14@gmail.com>
2021-08-04 18:50:58 +0300

    Create circle-floating image button contains profile photo

--- app/src/main/java/com/myfridge/ui/MainFragment.kt
+++ app/src/main/java/com/myfridge/ui/MainFragment.kt
@@ -1,7 +1,6 @@
 package com.myfridge.ui
 
 
-
 import android.net.Uri
 import android.os.Bundle
 import android.view.View
@@ -12,6 +11,7 @@ import com.myfridge.ui.main.MainActivity
 import com.myfridge.util.DownloadImage
 import com.myfridge.util.UserSettings
 import com.myfridge.viewModel.MainActivityViewModel
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_main.*
 import java.io.File
 
@@ -22,21 +22,22 @@ class MainFragment : Fragment(R.layout.fragment_main) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mainActivity = activity as MainActivity
+        //mainActivity = activity as MainActivity
 
+        //observeAccount()
+
+    }
+
+    private fun observeAccount() {
         mainActivityViewModel.account.observe(viewLifecycleOwner, { account ->
-
-            if (UserSettings.getFirstGoogleLogin(requireContext()) && UserSettings.getFirstStart(requireContext())) {
+            if (UserSettings.getFirstGoogleLogin(requireContext()) && UserSettings.getFirstStart(
+                    requireContext()
+                )
+            ) {
                 val path = DownloadImage(account.imagePath, requireContext()).execute("").get()
                 account.imagePath = path.toString()
             }
-            
-            profile_photo.setImageURI(Uri.fromFile(File(account.imagePath)))
-            mainTextView.setText(account.toString())
+            mainActivity.profile_circle_image.setImageURI(Uri.fromFile(File(account.imagePath)))
         })
-
-        profileButton.setOnClickListener {
-
-        }
     }
 }
--- app/src/main/java/com/myfridge/ui/main/MainActivity.kt
+++ app/src/main/java/com/myfridge/ui/main/MainActivity.kt
@@ -1,17 +1,28 @@
 package com.myfridge.ui.main
 
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.myfridge.R
+import com.myfridge.util.DownloadImage
+import com.myfridge.util.UserSettings
+import com.myfridge.viewModel.MainActivityViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.File
 
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
+    private val mainActivityViewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        observeAccount()
         setupBottomNavigation()
     }
 
@@ -22,5 +33,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         bottom_navigation.setupWithNavController(navController)
     }
 
+    private fun observeAccount() {
+        mainActivityViewModel.account.observe(this, { account ->
+            if (UserSettings.getFirstGoogleLogin(this) && UserSettings.getFirstStart(this)
+            ) {
+                val path = DownloadImage(account.imagePath, this).execute("").get()
+                account.imagePath = path.toString()
+            }
+           profile_circle_image.setImageURI(Uri.fromFile(File(account.imagePath)))
+        })
+    }
 }
 
